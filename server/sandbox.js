2013da2855c22cfaaff24d6c2099a8ce30a00245
alozor <nicolas-mery@neuf.fr>
2021-02-08 16:27:29 +0100

    feat(shop): scrape dedicated into json

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,21 +1,46 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const fs = require('fs');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const DEDICATED_BRAND = 'https://www.dedicatedbrand.com';
+
+async function sandbox() {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+    let allProducts = [];
+    let count = 0;
+    const pages = await dedicatedbrand.scrapeLinks();
+    for (const page of pages) {
+      console.log(`🕵️‍♀️  browsing ${page} source`);
+      const products = await dedicatedbrand.scrape(page);
+      // console.log(products);
+      console.log('done');
+      count += products.length;
+      products.forEach(product => product.brand = 'DEDICATED');
+      allProducts = allProducts.concat(products);
+    }
 
-    const products = await dedicatedbrand.scrape(eshop);
-
-    console.log(products);
-    console.log('done');
-    process.exit(0);
+    const json = JSON.stringify(allProducts, null, 2);
+    fs.writeFile('products.json', json, 'utf8', () => {
+      console.log(count);
+      process.exit(0);
+    });
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 }
 
-const [,, eshop] = process.argv;
+const [, , eshop] = process.argv;
+
+async function getPages() {
+  let count = 0;
+  const pages =
+    await dedicatedbrand.scrapeLinks();
+  for (const page of pages) {
+    count += await sandbox(page);
+  }
+  console.log(count);
+  process.exit(0);
+}
 
 sandbox(eshop);
--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const DEDICATED_BRAND = 'https://www.dedicatedbrand.com';
+
 /**
  * Parse webpage e-shop
  * @param  {String} data - html response
@@ -29,7 +31,7 @@ const parse = data => {
 
 /**
  * Scrape all the products for a given url page
- * @param  {[type]}  url
+ * @param  {string}  url
  * @return {Array|null}
  */
 module.exports.scrape = async url => {
@@ -44,3 +46,52 @@ module.exports.scrape = async url => {
 
   return null;
 };
+
+
+const parseLinks = data => {
+  const $ = cheerio.load(data);
+
+  return $('.mainNavigation-fixedContainer .mainNavigation-link-subMenu-link')
+    .map((i, element) => {
+      const link =  $(element)
+        .find('a')
+        //.find('.mainNavigation-link-subMenu-link > a[href]')
+        .attr('href');
+
+      return `${DEDICATED_BRAND}${link}`;
+    })
+    .get();
+};
+
+
+module.exports.scrapeLinks = async (url = DEDICATED_BRAND) => {
+  const response = await axios(url);
+  const {data, status} = response;
+
+  if (status >= 200 && status < 300) {
+    return parseLinks(data);
+  }
+
+  console.error(status);
+
+  return null;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
