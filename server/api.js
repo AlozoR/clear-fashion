71d85272c9afb6e4877395ceb7392ccc9443d59f
alozor <nicolas-mery@neuf.fr>
2021-04-05 16:52:54 +0200

    feat: endpoint products/search

--- server/api.js
+++ server/api.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const db = require('./db');
 
 const PORT = 8092;
 
@@ -16,6 +17,23 @@ app.options('*', cors());
 
 app.get('/', (request, response) => {
   response.send({'ack': true});
+});
+
+app.get('/products/search', async (request, response) => {
+  const limit = request.query.limit ? parseInt(request.query.limit) : 12;
+  let mongoQuery = {};
+  if (request.query.brand) {
+    mongoQuery.brand = request.query.brand;
+  }
+  if (request.query.price) {
+    mongoQuery.price = { $lt: parseInt(request.query.price) };
+  }
+  const sort = {price: 1};
+  const result = {limit: limit};
+  const mongoResult = await db.findSortAndLimit(mongoQuery, sort, limit);
+  result.total = mongoResult.length;
+  result.results = mongoResult;
+  response.send(result);
 });
 
 app.listen(PORT);
